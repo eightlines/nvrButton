6590e11fa62f569fb40d8e339e3ce4f724fed8fe
Brent Marshall <bmarshall@deeplocal.com>
2024-06-05 23:00:49 -0400

    add keypress monitoring

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import robot from 'robotjs'
+import GK from 'global-keypress'
+
+const gk = new GK()
+
+gk.start(); // note: requires sudo to run
+gk.on('press', data => {
+    if (data.data === '/') { // press / to exit the program (global hotkey)
+        gk.stop();
+        process.exit(0);
+    }
+
+    if (data.data === 'K') {
+        console.log('pressed K')
+        main()
+    }
+});
 
 const main = () => {
-    robot.moveMouse(100, 100)
+    robot.moveMouse(1000, 400) // Set this to the X/Y coordinates of the button you want to click
     robot.mouseClick()
 }
-
-main()
